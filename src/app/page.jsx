03b8db9b54b198abdae31b949e14e4cada4a0fa5
Orloxx23 <madorlix@gmail.com>
2024-01-23 16:09:33 -0500

    Remove unused code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,9 @@ import Background from "@/components/Background";
 import DiscordTime from "@/components/sections/DiscordTime";
 import Timezones from "@/components/sections/Timezones";
 import Footer from "../components/Footer";
-import { showEasterEgg } from "../lib/easteregg";
 
 export default function Home() {
   const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    showEasterEgg();
-  }, []);
 
   return (
     <main className="w-full min-h-screen flex flex-col gap-8 justify-center items-center relative">
